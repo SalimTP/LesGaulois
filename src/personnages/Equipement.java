fede3c87b05aaacf6fc3a6cbf6151dfdd073718c
moussaoui salim <mss4170a@ens.fsi>
2022-10-17 15:47:23 +0200

    TP2

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,20 @@
+package personnages;
+
+public enum Equipement {
+    
+    
+    CASQUE("casque"),
+    BOUCLIER("bouclier");
+    private String nom=" ";
+    
+    
+    private Equipement(String chaine) {
+        this.nom=chaine;
+    }    
+    
+    public String toString() {
+        return this.nom;
+    }
+    
+
+}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,25 +3,22 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
+	private Equipement [] equipements = new Equipement[2];
+	private int nbEquipement=0;
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		assert (force>0);
 			this.force = force;
 	}
-	
 	public String getNom() {
 		return nom;
 	}
-	
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
-	
 	private String prendreParole() {
 		return "Le romain " + nom + " : ";
 	}
-	
 	public void recevoirCoup(int forceCoup) {
 		assert (force>0);
 		int forceTemp=force-forceCoup;
@@ -33,14 +30,55 @@ public class Romain {
 		}
 		assert(force==forceTemp);
 	}
+	public void sEquiper(Equipement equip) {
+		switch(nbEquipement) {
+		case 2:
+			System.out.println("Le soldat "+ this.nom+" est déjà bien protégé !");
+			break;
+		case 1:
+			if(this.equipements[0].toString()==equip.toString() )
+				System.out.println("Le soldat "+this.nom+" possède déjà un "+ this.equipements[0].toString()+ " !");
+			else {
+				switchPrvivate(equip);
+			}
+			break;
+		case 0:
+			switchPrvivate(equip);
+			break;
+		}
+			
+			
+		
+	}
+	private void switchPrvivate(Equipement equip) {
+		if("casque"==equip.toString())
+			System.out.println("Le soldat "+this.nom+" s’équipe avec un casque !");
+		else
+			System.out.println("Le soldat "+this.nom+" s’équipe avec un bouclier !");
+		equipements[nbEquipement]=equip;	
+		nbEquipement++;
+	}
 	
 	public static void main(String[] args) {
-		Romain rom = new Romain("Brutus",6);
+		Romain rom = new Romain("Brutus",4);
 		//on verifie le bon fonctionnement de la méthode prendreParole
 		System.out.println(rom.prendreParole());
 		//on verifie le bon fonctionnement de la méthode parler
 		rom.parler("Longue vie à Rome !");
 		//on verifie le bon fonctionnement de la méthode recevoirCoup
 		rom.recevoirCoup(2);
+		
+		//TP2
+		Equipement equi = Equipement.CASQUE; 		
+	    System.out.println(equi);	 
+	    equi = Equipement.BOUCLIER;
+	    Romain minus = new Romain("Minus",4);
+	    equi = Equipement.CASQUE;
+	    minus.sEquiper(equi);
+	    minus.sEquiper(equi);
+	    equi = Equipement.BOUCLIER;
+	    minus.sEquiper(equi);
+	    equi = Equipement.CASQUE;
+	    minus.sEquiper(equi);
 	}
 }
